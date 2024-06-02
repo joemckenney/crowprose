1e06eb56d5f601a6451c8cc06a59320588dcb350
Joe McKenney <joe@switchboard.cc>
2024-06-02 11:04:17 -0700

    feat(@www/app): add DIN 1451 font

--- apps/@www/app/src/main.tsx
+++ apps/@www/app/src/main.tsx
@@ -2,6 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 
+import "../font/din-1451/DINEngschriftStd.woff";
+import "../font/din-1451/DINMittelschriftStd.woff";
+
 import { Routes } from "./routes";
 
 import { BrowserRouter } from "react-router-dom";
